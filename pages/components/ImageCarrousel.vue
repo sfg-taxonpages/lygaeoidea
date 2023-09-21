389337af1549e31ed07dff0d59a39a63186903e9
jlpereira <pepelui@outlook.com>
2023-09-21 10:27:01 -0300

    Update template

---
+++ pages/components/ImageCarrousel.vue
@@ -0,0 +1,154 @@
+<template>
+  <div class="overflow-hidden h-[550px] w-full relative">
+    <img
+      class="object-cover overflow-hidden h-[550px] w-full absolute my-0"
+      :key="currentImage.src"
+      :src="currentImage.src"
+      alt="Dichroplus maculipennis"
+    />
+    <div class="bg-black bg-opacity-25 absolute h-full w-full top-0">
+      <slot />
+    </div>
+    <div class="absolute bottom-2 right-4">
+      <span class="z-10 text-white text-sm drop-shadow">
+        <RouterLink
+          v-if="currentImage.otuId"
+          class="text-white decoration-transparent"
+          :to="{ name: 'otus-id', params: { id: currentImage.otuId } }"
+        >
+          <i>{{ currentImage.label }}</i> © {{ currentImage.copyright }}
+        </RouterLink>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { onMounted, ref, computed } from 'vue'
+
+import Artemidorus from './images/Artemidorus sp.(Heterogastridae) (c) luluchouette.jpeg'
+import beosusQuadripunctatus from './images/Beosus quadripunctatus (c) Dimitǎr Boevski.jpeg'
+import horridipameraRobusta from './images/Horridipamera robusta (c) Tom.jpeg'
+import oxycarenusLavaterae from './images/Oxycarenus lavaterae (c) Juraj Ahel.jpg'
+import oxycarenusLavateraeJens from './images/Oxycarenus lavaterae (c) Jens Oldenburg.jpg'
+import ragliusConfusus from './images/Raglius confusus (c) Raglius confusus.jpeg'
+import lygaeusTurcicus from './images/Lygaeus turcicus (c) Lygaeus turcicus.jpeg'
+import oedancalaDorsalis from './images/Oedancala Dorsalis (c) Steven Lamonde.jpeg'
+import jalysus from './images/Jalysus (c) Adrienne van den Beemt.jpg'
+import oncopeltusFasciatus from './images/Oncopeltus fasciatus (c) Jay Brasher.jpg'
+import rhyparochromusPini from './images/Rhyparochromus pini (c) Paweł Ryszawa.jpg'
+import oedancalaCrassimana from './images/Oedancala crassimana (c) Joshua Soileau.jpeg'
+import oncopeltus from './images/Oncopeltus sp (c) DiegoH.jpg'
+
+const props = defineProps({
+  duration: {
+    type: Number,
+    default: 5000
+  }
+})
+
+const images = [
+  {
+    label: 'Artemidorus sp.(Heterogastridae)',
+    copyright: 'luluchouette',
+    src: Artemidorus,
+    otuId: 819322
+  },
+  {
+    label: 'Beosus quadripunctatus',
+    copyright: 'Dimitǎr Boevski',
+    src: beosusQuadripunctatus,
+    otuId: 850435
+  },
+  {
+    label: 'Horridipamera robusta',
+    copyright: 'Tom',
+    src: horridipameraRobusta,
+    otuId: 848543
+  },
+  {
+    label: 'Oxycarenus lavaterae',
+    copyright: 'Juraj Ahel',
+    src: oxycarenusLavaterae,
+    otuId: 831633
+  },
+  {
+    label: 'Oxycarenus lavaterae',
+    copyright: 'Jens Oldenburg',
+    src: oxycarenusLavateraeJens,
+    otuId: 816243
+  },
+  {
+    label: 'Raglius confusus',
+    copyright: 'Raglius confusus',
+    src: ragliusConfusus,
+    otuId: 850176
+  },
+  {
+    label: 'Lygaeus turcicus',
+    copyright: 'Lygaeus turcicus',
+    src: lygaeusTurcicus,
+    otuId: 850075
+  },
+  {
+    label: 'Oedancala Dorsalis',
+    src: oedancalaDorsalis,
+    copyright: 'Steven Lamonde',
+    otuId: 823304
+  },
+  {
+    label: 'Jalysus',
+    src: jalysus,
+    copyright: 'Adrienne van den Beemt',
+    otuId: 816353
+  },
+  {
+    label: 'Oncopeltus fasciatus',
+    src: oncopeltusFasciatus,
+    copyright: 'Jay Brasher',
+    otuId: 823314
+  },
+  {
+    label: 'Rhyparochromus pini',
+    copyright: 'Paweł Ryszawa',
+    src: rhyparochromusPini,
+    otuId: 820284
+  },
+  {
+    label: 'Oedancala crassimana',
+    copyright: 'Joshua Soileau',
+    src: oedancalaCrassimana,
+    otuId: 823378
+  },
+  {
+    label: 'Oncopeltus sp (c)',
+    copyright: 'DiegoH',
+    src: oncopeltus,
+    otuId: 823378
+  }
+]
+
+const currentIndex = ref(null)
+const currentImage = computed(() => images[currentIndex.value] || {})
+
+onMounted(() => {
+  currentIndex.value = Math.floor(Math.random() * images.length)
+})
+</script>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 1s ease-in-out;
+}
+.fade-enter-from {
+  opacity: 0;
+}
+.fade-enter-to {
+  opacity: 1;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
--- pages/home.md
+++ pages/home.md
@@ -1,26 +1,51 @@
 ---
 # See project.yml for variables.
+layout: blank
 ---
 
-# {{ app:project_name }}
-The Lygaeoidea Species File (LSF) is a taxonomic database of the world’s seed bugs, stilt bugs, chinch bugs, big-eyed bugs and relatives, both living and fossil. It has full taxonomic and synonymic information for all taxa, with complete taxonomic references, and images, specimen records and other useful resources are constantly being added.
+<div class="bg-base-foreground">
+  <ImageCarrousel>
+    <div class="flex flex-col justify-center items-center w-full h-full bg-black bg-opacity-25 gap-4">
+      <span class="text-4xl font-medium text-white">{{ app:project_name }}</span>
+      <span class="text-xl text-white">The Lygaeoidea Species File (LSF) is a taxonomic database of the world’s seed bugs, stilt bugs, chinch bugs, big-eyed bugs and relatives</span>
+      <div class="p-4 bg-black bg-opacity-25 w-full max-w-lg box-border">
+        <AutocompleteOtu
+          autofocus
+          class="w-full"
+          placeholder="Search by taxon name..."
+        />
+      </div>
+    </div>
+  </ImageCarrousel>
+  <div class="container mx-auto pb-10">
+    <SectionData/>
+
+The {{ app:project_name }} (LSF) is a taxonomic database of the world’s seed bugs, stilt bugs, chinch bugs, big-eyed bugs and relatives, both living and fossil. It has full taxonomic and synonymic information for all taxa, with complete taxonomic references, and images, specimen records and other useful resources are constantly being added.
 
 The core purpose of the LSF is to provide a resource that makes research on the group more efficient as well as to cover the needs of policy-makers, environmental managers and the wider public for a consistent and up-to-date classification of the world’s Lygaeoidea species.
 
-<autocomplete-otu class="w-80 place-content-center" placeholder="Search by taxon name"/>
-
 ## Authors
-Pablo M. Dellapé,	División Entomología, Museo de La Plata, Argentina	[ORCID](https://orcid.org/0000-0002-6914-1026)
 
-Thomas J. Henry,	Systematic Entomology Laboratory, ARS, USDA c/o National Museum of Natural History, Smithsonian Institution, Washington, DC	[ORCID](https://orcid.org/0000-0002-0653-7728)
+Pablo M. Dellapé, División Entomología, Museo de La Plata, Argentina [ORCID](https://orcid.org/0000-0002-6914-1026)
 
-David C. Eades*,	(Historical) Principal Database Developer	Illinois Natural History Survey	
+Thomas J. Henry, Systematic Entomology Laboratory, ARS, USDA c/o National Museum of Natural History, Smithsonian Institution, Washington, DC [ORCID](https://orcid.org/0000-0002-0653-7728)
 
-An * after someone’s name denotes a past contributor, now inactive.
+David C. Eades\*, (Historical) Principal Database Developer Illinois Natural History Survey
+
+An \* after someone’s name denotes a past contributor, now inactive.
 
 ## Discover more
-Find out more on this new public version of LSF. Learn how to collaborate with the project. Contact information, technical details, and more are available at  [About](about) 
 
+Find out more on this new public version of LSF. Learn how to collaborate with the project. Contact information, technical details, and more are available at [About](about)
 
 ## Announcements
+
 August 2023 - Welcome to Our New Lygaeoidea Species File Website. Please enjoy these new ways to access our Lygaeoidea data. We look forward to your insights, participation, use of our data, and your contributions.
+
+  </div>
+</div>
+
+<script setup>
+import ImageCarrousel from './components/ImageCarrousel.vue'
+import SectionData from './components/Section/SectionData.vue'
+</script>
